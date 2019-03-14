fdaf6cae4da6a53cb50c89838ca9da50f04e48e4
konstantin <konstantin.tretikov@studiomobile.ru>
2019-03-14 16:46:10 +0300

    Fix many mistakes :)

--- Makefile
+++ Makefile
@@ -1,10 +1,15 @@
 CC=clang
 CFLAGS=-std=c99 -Wall -pedantic
+LFLAGS=
 
-all: build logsRemove
+all: export build logsRemove
 
-build: 
-	$(CC) $(CFLAGS) *.c
+export:
+    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/Users/tretikoff/Desktop/pa2";
+    LD_PRELOAD=/Users/tretikoff/Desktop/pa2/lib64/libruntime.so ./a.out –p 2 10 20;
+
+build:
+	$(CC) $(CFLAGS) *.c $(LFLAGS)
 
 logsRemove:
 	rm -r *.log
--- pa23.c
+++ pa23.c
@@ -16,11 +16,6 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-void transfer(void *parent_data, local_id src, local_id dst,
-              balance_t amount) {
-    // student, please implement me
-}
-
 int main(int argc, char *argv[]) {
     //bank_robbery(parent_data);
     //print_history(all);
@@ -28,25 +23,25 @@ int main(int argc, char *argv[]) {
     // fetch args
     getopt(argc, argv, "p:");
     int proc_count = atoi(optarg);
-    int balances[proc_count];
-    for (int i = 0; i < proc_count; ++i)
-        balances[i] = atoi(argv[i + 3]);
+    int balances[proc_count + 1];
+    for (int i = 1; i <= proc_count; ++i)
+        balances[i] = atoi(argv[i + 2]);
 
     InputOutput io;
     io.procCount = proc_count;
     io.fds = (int ***) calloc((proc_count + 1), sizeof(int **));
-//
-//    FILE *pipes_logfile = fopen(pipes_log, "a+");
-//    for (int i = 0; i <= proc_count; ++i) {
-//        io.fds[i] = (int **) calloc((proc_count + 1), sizeof(int *));
-//        for (int j = 0; j <= proc_count; ++j) {
-//            if (i == j) continue;
-//            io.fds[i][j] = (int *) calloc(2, sizeof(int));
-//            pipe(io.fds[i][j]);
-//            fprintf(pipes_logfile, "%d %d was opened\n", i, j);
-//        }
-//    }
-//
+
+    FILE *pipes_logfile = fopen(pipes_log, "a+");
+    for (int i = 0; i <= proc_count; ++i) {
+        io.fds[i] = (int **) calloc((proc_count + 1), sizeof(int *));
+        for (int j = 0; j <= proc_count; ++j) {
+            if (i == j) continue;
+            io.fds[i][j] = (int *) calloc(2, sizeof(int));
+            pipe(io.fds[i][j]);
+            fprintf(pipes_logfile, "%d %d was opened\n", i, j);
+        }
+    }
+
     FILE *logfile;
     logfile = fopen(events_log, "a+");
 
@@ -59,20 +54,20 @@ int main(int argc, char *argv[]) {
             history.s_id = i;
             SelfInputOutput sio = {io, i};
             close_pipes(&sio, i);
-//
-//            fprintf(logfile, log_started_fmt, i, getpid(), getppid());
-//            fflush(logfile);
-//
-//            Message msg;
-//            sprintf(msg.s_payload, log_started_fmt, i, getpid(), getppid());
-//            createMessageHeader(&msg, STARTED);
-//
-//            send_multicast(&sio, &msg);
-//
-//            Message msgs[proc_count + 1];
-//            receive_all(&sio, msgs, STARTED);
-//            fprintf(logfile, log_received_all_started_fmt, i);
-//            fflush(logfile);
+
+            fprintf(logfile, log_started_fmt, get_physical_time(), i, getpid(), getppid(), balances[i]);
+            fflush(logfile);
+
+            Message msg;
+            sprintf(msg.s_payload, log_started_fmt, get_physical_time(), i, getpid(), getppid(), balances[i]);
+            createMessageHeader(&msg, STARTED);
+
+            send_multicast(&sio, &msg);
+
+            Message msgs[proc_count + 1];
+            receive_all(&sio, msgs, STARTED);
+            fprintf(logfile, log_received_all_started_fmt, get_physical_time(), i);
+            fflush(logfile);
 //
 //            Message msg2;
 //            sprintf(msg2.s_payload, log_done_fmt, i);
@@ -82,14 +77,14 @@ int main(int argc, char *argv[]) {
 //            send_multicast(&sio, &msg2);
 //
 //            receive_all(&sio, msgs, DONE);
-//            fprintf(logfile, log_received_all_done_fmt, i);
+//            fprintf(logfile, log_received_all_done_fmt, get_physical_time(), i);
 //
 
 
-            Message msg;
+//            Message msgHistory;
 //            msg.s_payload = history;
-            createMessageHeader(&msg, BALANCE_HISTORY);
-            send(&sio, 0, &msg);
+//            createMessageHeader(&msgHistory, BALANCE_HISTORY);
+//            send(&sio, 0, &msgHistory);
             return 0;
         }
     }
@@ -98,18 +93,29 @@ int main(int argc, char *argv[]) {
     close_pipes(&sio, 0);
     Message msgs[proc_count + 1];
     receive_all(&sio, msgs, STARTED);
-    fprintf(logfile, log_received_all_started_fmt, 0);
-    bank_robbery()
+    fprintf(logfile, log_received_all_started_fmt, get_physical_time(), 0);
+//    TODO parent data
+    int max_id = proc_count - 1;
+    bank_robbery(&sio, max_id);
 
-    Message msg;
-    createMessageHeader(&msg, STOP);
-    send_multicast(sio, msg);
+    Message doneMsg;
+    createMessageHeader(&doneMsg, STOP);
+    send_multicast(&sio, &doneMsg);
     receive_all(&sio, msgs, DONE);
-//    fflush(logfile);
-//    fprintf(logfile, log_received_all_done_fmt, 0);
-//    for (int i = 0; i < sio.io.procCount; i++)
-//        wait(NULL);
-//    fflush(logfile);
+    fflush(logfile);
+    fprintf(logfile, log_received_all_done_fmt, get_physical_time(), 0);
+    for (int i = 0; i < sio.io.procCount; i++)
+        wait(NULL);
+    fflush(logfile);
 
     return 0;
+}
+
+void transfer(void *parent_data, local_id src, local_id dst, balance_t amount) {
+    // student, please implement me
+}
+
+//TODO remove
+timestamp_t get_physical_time() {
+    return 1;
 }
